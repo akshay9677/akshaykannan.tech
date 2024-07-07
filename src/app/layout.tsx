86a115fffee085f49afcf96d9a915f88cd70b43d
akshay-facilio <akshay@facilio.com>
2024-07-07 21:14:18 +0530

    Added twitter image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,23 @@ export default function RootLayout({
         isDark ? "theme-dark" : "theme-light"
       } bg-container-primary text-content-primary transition-colors overflow-hidden`}
     >
+      <head>
+        <meta property="og:image" content="<generated>" />
+        <meta property="og:image:type" content="<generated>" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" />
+
+        <meta name="twitter:image" content="<generated>" />
+        <meta name="twitter:image:type" content="<generated>" />
+        <meta name="twitter:image:width" content="<generated>" />
+        <meta name="twitter:image:height" content="<generated>" />
+        <meta name="twitter:site" content="@_akshay_kannan_" />
+        <meta name="twitter:title" content="Akshay Kannan" />
+        <meta
+          name="twitter:description"
+          content="Personal portfolio of Akshay Kannan"
+        />
+      </head>
       <body className="relative h-screen w-full">
         <div className="h-screen overflow-scroll pb-20">
           <div className="w-full flex justify-center">
